e353b3822fefc23600f15ec0b07ae8af9cc80e90
MyconPL <ziomus12.07@gmail.com>
2026-06-09 18:35:36 +0200

    created class Student

---
+++ PPO_projekt.Domain/Entites/Student.cs
@@ -0,0 +1,36 @@
+﻿namespace PPO_projekt.Domain.Entites;
+
+public class Student
+{
+    public string Name { get; private  set; }
+    private readonly List<int> _grades = new();
+
+    public Student(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentException("Name cannot be empty.");
+        }
+        Name = name;
+    }
+    
+    public void AddGrade(int grade)
+    {
+        if (grade < 1 || grade > 6)
+        {
+            throw new ArgumentException("Grade must be between 1 and 6.");
+        }
+        _grades.Add(grade);
+    }
+
+    public IReadOnlyList<int> GetGrades()
+    {
+        return _grades.AsReadOnly();
+    }
+
+    public double GetAverage()
+    {
+        if (_grades.Count == 0) return 0;
+        return _grades.Average();
+    }
+}
