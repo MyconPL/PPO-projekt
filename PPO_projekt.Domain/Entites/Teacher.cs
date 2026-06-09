3d071e83299b11e773d6fa8bb862211ff886d98f
MyconPL <ziomus12.07@gmail.com>
2026-06-09 19:02:54 +0200

    created class Teacher

---
+++ PPO_projekt.Domain/Entites/Teacher.cs
@@ -0,0 +1,32 @@
+﻿namespace PPO_projekt.Domain.Entites;
+
+public class Teacher
+{
+    public string Name { get; private set; }
+
+    private readonly List<Subject> _subjects = new();
+
+    public Teacher(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Teacher name cannot be empty.");
+
+        Name = name;
+    }
+
+    public void AddSubject(Subject subject)
+    {
+        if (subject == null)
+            throw new ArgumentNullException(nameof(subject));
+
+        if (_subjects.Contains(subject))
+            return;
+
+        _subjects.Add(subject);
+    }
+
+    public IReadOnlyList<Subject> GetSubjects()
+    {
+        return _subjects.AsReadOnly();
+    }
+}
