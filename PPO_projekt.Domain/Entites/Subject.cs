999a439a9d037c1fc5e261cd3f0cd719cce34403
MyconPL <ziomus12.07@gmail.com>
2026-06-09 19:03:04 +0200

    created class Subject

---
+++ PPO_projekt.Domain/Entites/Subject.cs
@@ -0,0 +1,32 @@
+﻿namespace PPO_projekt.Domain.Entites;
+
+public class Subject
+{
+    public string Name { get; set; }
+    
+    private readonly List<Student> _students = new();
+    
+    public Subject(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Subject name cannot be empty.");
+
+        Name = name;
+    }
+
+    public void AddStudent(Student student)
+    {
+        if (student == null)
+            throw new ArgumentNullException(nameof(student));
+
+        if (_students.Contains(student))
+            return;
+
+        _students.Add(student);
+    }
+
+    public IReadOnlyList<Student> GetStudents()
+    {
+        return _students.AsReadOnly();
+    }
+}
